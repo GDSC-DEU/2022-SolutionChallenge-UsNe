d1d12477dab668b2159804fc93aa00effee77f51
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-02-11 15:08:20 +0900

    Feat:Entity

--- backend/src/main/java/com/gdsc/backend/domain/ConsumptionGoal.java
+++ backend/src/main/java/com/gdsc/backend/domain/ConsumptionGoal.java
@@ -2,9 +2,32 @@ package com.gdsc.backend.domain;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class ConsumptionGoal {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long index;
+
+    @Column
+    private int price;
+
+    @Column
+    private LocalDateTime datetime;
+
+    @ManyToOne
+    private User user;
+
+    @Builder
+    public ConsumptionGoal(int price, User user){
+        this.price = price;
+        this.user = user;
+    }
 }
