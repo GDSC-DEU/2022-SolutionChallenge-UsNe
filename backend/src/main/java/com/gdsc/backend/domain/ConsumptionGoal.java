26cfac4a34c51bc638a4a116c4c48169982c2d89
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-02-11 19:12:33 +0900

    Feat:entity

--- backend/src/main/java/com/gdsc/backend/domain/ConsumptionGoal.java
+++ backend/src/main/java/com/gdsc/backend/domain/ConsumptionGoal.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class ConsumptionGoal {
     @Id
     @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long index;
 
     @Column(nullable = false)
