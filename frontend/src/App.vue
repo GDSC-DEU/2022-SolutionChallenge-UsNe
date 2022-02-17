e1b1e1adde02307313f22c32f69bd5eac1871f37
Shin Dong-Ha <91720974+Shin-Dong-Ha@users.noreply.github.com>
2022-02-17 16:21:32 +0900

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,9 +31,38 @@
           <td>{{ list.tag.join(", ") }}</td>
           <td>{{ list.inout }}</td>
           <td>{{ list.total }}</td>
-        </tr>
+        </tr> 
       </tbody>
     </table>
+    <!-- <head>
+       <meta name="viewport" content="width=device-width">
+      <title>replit</title>
+    </head> -->
+    <input type="text">
+    <h2>replit</h2>
+    <div class="main">
+      <div class="title">
+        Goal
+      </div>
+      <div class="subtitle">
+        한 달 예산 <br><input class="subtitle-cost"> 원
+      <button class="button-set">
+      설정
+      </button>
+      </div>
+    <div class="account"> 
+     <p>식비<input class="account-cost"> 
+        문화<input class="account-cost"></p>
+     <p>생활<input class="account-cost">
+        의류<input class="account-cost"></p> 
+     <p>쇼핑<input class="account-cost">  
+        미용<input class="account-cost"></p> 
+     <p>금융<input class="account-cost">   
+        의료<input class="account-cost"></p>     
+      </div>
+      <button class="button-input">Input</button>
+      <button class="button-cancel">cancel</button>
+    </div>
     <Modal 
       v-if = "showNewModal"
       @close="closeNewHodal"
@@ -87,6 +116,55 @@ export default {
 </script>
 
 <style>
+.main {
+        text-align: center;
+        width: 800px;
+        margin: 0 auto;
+        font-family: inherit;
+        font-size: inherit;
+        line-height: inherit;
+      }
+      .title {
+        text-align: center;
+        font-size: 2rem;
+        font-weight: bold;
+        margin-top: 60px;
+      }
+
+      subtitle {
+        text-align: center;
+        font-size: 1rem;
+        font-weight: bold;
+        margin-top: 30px;
+      }   
+
+      account {
+        text-align: center;
+        margin-top: 100px;
+      }
+     
+      #button-set, #button-input, #button-cancel {
+        width: 55px;
+        padding: 4px;
+        margin: 5px;
+        border-radius: 35px;
+        border: none;
+        color: white;
+        position: relative;
+        top: -5px;
+        left: 68%;  
+      }
+        
+      #button-set {
+        color: black
+      }
+      #button-input {
+       background-color: rgb(100, 170, 235);
+      }
+     #button-cancel {
+        background-color: rgb(255, 160, 160);
+     }
+     
   * {
     font-family:serif; 
   }
