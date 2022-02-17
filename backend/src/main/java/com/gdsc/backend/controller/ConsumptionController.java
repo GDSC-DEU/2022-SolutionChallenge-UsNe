a2501ddfd0aca6ccf56f93a44e3515c3f6faf469
sudhdkso <waga1009@gmail.com>
2022-02-17 22:45:07 +0900

    Fix:함수반환형통일(#20)

--- backend/src/main/java/com/gdsc/backend/controller/ConsumptionController.java
+++ backend/src/main/java/com/gdsc/backend/controller/ConsumptionController.java
@@ -1,7 +1,11 @@
 package com.gdsc.backend.controller;
 
 import com.gdsc.backend.domain.Consumption;
+import com.gdsc.backend.repository.ConsumptionRepository;
 import com.gdsc.backend.service.ConsumptionManagementService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,29 +23,32 @@ public class ConsumptionController {
     }
 
     @PostMapping
-    public ResponseEntity<Consumption> createConsumption(@RequestBody Consumption consumption){
-        Consumption creation=consumptionManagementService.save(consumption);
-        return ResponseEntity.ok().body(consumption);
+    public ResponseEntity<?> createConsumption(@RequestBody Consumption consumption){
+        Consumption creation= consumptionManagementService.save(consumption);
+        return new ResponseEntity<>(creation, HttpStatus.OK);
     }
 
     @GetMapping
-    public List<Consumption> showConsumptionList(){
-        return consumptionManagementService.showAll();
+    public ResponseEntity<?> showConsumptionList(final Pageable pageable){
+        Page<Consumption> consumptions= consumptionManagementService.showConsumptionsByPage(pageable);
+        return new ResponseEntity<>(consumptions, HttpStatus.OK);
     }
 
     @GetMapping("/{index}")
-    public Optional<Consumption> showConsumptionByIndex(@PathVariable("index")Long index){
-        return consumptionManagementService.showByIndex(index);
+    public ResponseEntity<?> showConsumptionByIndex(@PathVariable("index")Long index){
+        Optional<Consumption> consumption= consumptionManagementService.showByIndex(index);
+        return new ResponseEntity<>(consumption, HttpStatus.OK);
     }
 
     @PutMapping("/{index}")
     public ResponseEntity<?> updateConsumption(@PathVariable("index")Long index, @RequestBody Consumption consumption){
         Consumption updation= consumptionManagementService.update(index, consumption);
-        return ResponseEntity.ok().body(updation);
+        return new ResponseEntity<>(updation, HttpStatus.OK);
     }
 
     @DeleteMapping("/{index}")
-    public void deleteConsumption(@PathVariable("index")Long index){
+    public ResponseEntity deleteConsumption(@PathVariable("index")Long index){
         consumptionManagementService.delete(index);
+        return new ResponseEntity<>("{}",HttpStatus.OK);
     }
 }
