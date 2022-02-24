46987b3928474ff7362b4336a1b92c1509f8f312
sudhdkso <waga1009@gmail.com>
2022-02-25 00:45:07 +0900

    Feat: 사용자를 식별하여 소비기능 작동

--- backend/src/main/java/com/gdsc/backend/repository/ConsumptionRepository.java
+++ backend/src/main/java/com/gdsc/backend/repository/ConsumptionRepository.java
@@ -2,12 +2,18 @@ package com.gdsc.backend.repository;
 
 import com.gdsc.backend.domain.Consumption;
 import com.gdsc.backend.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ConsumptionRepository extends JpaRepository<Consumption,Long> {
     public Consumption findConsumptionByConsumptionIndex(Long index);
 
     public Consumption findConsumptionByConsumptionIndexAndAndUser(Long index, User user);
+
+    public List<Consumption> findAllByUser(Pageable pageable,User user);
+
 }
