c79e404613923ef8b1b7210289f1d10c685704aa
yuw0l <cjy020718@gmail.com>
2022-03-23 22:28:23 +0900

    Feat: 커뮤니티 그룹 생성, 그룹 조회

--- frontend/src/pages/community/index.vue
+++ frontend/src/pages/community/index.vue
@@ -2,7 +2,11 @@
   <div>
     <h2>My Community</h2>
     <div class="groupBar">
-      <button>
+      <div class="inputGroup" v-show="inputGroupname">
+        <input type="text" v-model="groupName">
+        <button @click="newGroup">생성하기</button>
+      </div>
+      <button @click="addGroup">
         <img class="groupIcon" src='@/images/user-group-solid.png'>
         그룹추가
       </button>
@@ -12,7 +16,7 @@
       </button>
     </div>
     <div class="groupbody">
-      <div class="group1">
+      <!-- <div class="group1">
        <a href="url"><img src="@/images/group-img.jpeg" border="0" width="200" height="200"></a>
        <p>절므니</p>
       </div>
@@ -27,19 +31,80 @@
       <div class="group4">
        <a href="url"><img src="@/images/img.jpg" border="0" width="200" height="200"></a>
        <p>돈사모</p>
+      </div> -->
+      <div class="myGrouplist">
+        <ul class="groupGrid">
+          <li v-for="(myGroup, index) in myGroups" :key="index">
+            <div class="group1">
+              <a href="url"><img src="@/images/group-img.jpeg" border="0" width="200" height="200"></a>
+            </div>
+            <p>{{ myGroup.name }}</p>
+          </li>
+        </ul>
+        <div style="clear:both;"></div>
+        <!-- {{ myGroups }} -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import { postNewGroup, getMyGroup } from "@/api/index.js";
 export default {
-  
+  mounted() {
+    this.loadingGroup()
+  },
+  methods: {
+    addGroup() {
+      if(this.inputGroupname==true) {
+        this.inputGroupname=false;
+      } else {
+        this.inputGroupname=true;
+      }
+    },
+    async newGroup() {
+      if(this.groupName == "") {
+        alert("그룹이름을 입력하세요")
+      } else {
+        const userData = {
+          name: this.groupName
+        }
+        const {data} = await postNewGroup(userData);
+        console.log(data);
+        this.groupName = ""
+        this.inputGroupname= false;
+        this.loadingGroup();
+      }
+    },
+    async loadingGroup() {
+      const response = await getMyGroup();
+      this.myGroups = response.data;
+      console.log(this.myGroups);
+    }
+  },
+  data() {
+    return {
+      groupName: "",
+      inputGroupname: false,
+      myGroups: [],
+    }
+  },
 }
 </script>
 
 <style scoped>
+  .myGrouplist {
+    display: flex;
+    justify-content:center;
+  }
+  .groupGrid {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+  }
+  ul {
+    max-width: 997px;
+  }
   #joinIcon {
     width: 12px;
     display: flex;
@@ -60,35 +125,7 @@ export default {
     border: 1px solid;
     width: 200px;
     height: 200px;
-    padding: 50;
-    margin-right: 20px;
-    margin-left: 500px;
-    float: left;
-  }
-  .group2 {
-    text-align: center;
-    border: 1px solid;
-    width: 200px;
-    height: 200px;
-    padding: 50;
-     margin-right: 20px;
-    float: left;
-  }
-  .group3 {
-    text-align: center;
-    border: 1px solid;
-    width: 200px;
-    height: 200px;
-    padding: 50;
-     margin-right: 20px;
-    float: left;
-  }
-  .group4 {
-    text-align: center;
-    border: 1px solid;
-    width: 200px;
-    height: 200px;
-    padding: 50;
+    margin: 10px;
     float: left;
   }
   .groupBar {
