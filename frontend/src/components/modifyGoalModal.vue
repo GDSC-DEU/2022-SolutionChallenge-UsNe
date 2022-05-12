cfe9596a4212b3cd14574997ddfa84339ec62aee
yuw0l <cjy020718@gmail.com>
2022-05-12 23:42:16 +0900

    Feat: 목표 수정

--- frontend/src/components/modifyGoalModal.vue
+++ frontend/src/components/modifyGoalModal.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="modalBody">
+<template> 
+  <div class="modalBody"> <!--목표 수정 모달창-->
     <h1>Goal</h1>
     <div v-show="errorMessage" style="color: red;">예산이 부족합니다.</div>
     <p>한달예산</p>
@@ -11,39 +11,70 @@
     <div>{{ totalConsumption }}</div>
     <div class="goalInput"></div>
     <p>
-      #식비<input type="number" v-model="food">  
-      #문화<input type="number" v-model="culture">
+      <span v-show="inputBtn">#식비</span>
+      <input type="number" v-model="food" v-show="inputBtn"> 
+      <span v-show="inputBtn">#문화</span>
+      <input type="number" v-model="culture" v-show="inputBtn">
     </p>
     <p>
-      #생활<input type="number" v-model="life">  
-      #의류<input type="number" v-model="clothes">
+      #식비{{ this.goals.food }}
+      #문화{{ this.goals.culture }}
     </p>
     <p>
-      #교육<input type="number" v-model="education">  
-      #의료<input type="number" v-model="medicaltreatment">
+      <span v-show="inputBtn">#생활</span>
+      <input type="number" v-model="life" v-show="inputBtn">  
+      <span v-show="inputBtn">#의류</span>
+      <input type="number" v-model="clothes" v-show="inputBtn">
     </p>
     <p>
-      #교통<input type="number" v-model="traffic">  
-      #뷰티<input type="number" v-model="beauty">
+      #생활{{ this.goals.life }}
+      #의류{{ this.goals.clothes }}
     </p>
-    <button type="button" @click="addbudget">입력</button>
+    <p>
+      <span v-show="inputBtn">#교육</span>
+      <input type="number" v-model="education" v-show="inputBtn">  
+      <span v-show="inputBtn">#의료</span>
+      <input type="number" v-model="medicaltreatment" v-show="inputBtn">
+    </p>
+    <p>
+      #교육{{ this.goals.education }}
+      #의료{{ this.goals.medicaltreatment }}
+    </p>
+    <p>
+      <span v-show="inputBtn">#교통</span>
+      <input type="number" v-model="traffic" v-show="inputBtn">  
+      <span v-show="inputBtn">#뷰티</span>
+      <input type="number" v-model="beauty" v-show="inputBtn">
+    </p>
+    <p>
+      #교통{{ this.goals.traffic }}
+      #뷰티{{ this.goals.beauty }}
+    </p>
+    <button type="button" @click="showInputBtn" v-show="modifyBtn">수정</button>
+    <button type="button" @click="putbudget" v-show="putbudgetBtn">설정</button>
     <button type="button" @click="$emit('close')">닫기</button>
-  </div> 
-</template> 
+  </div>
+</template>
 
 <script>
+import { getGoals } from "@/api/index";
 export default {
+  mounted() {
+    // 한달 예산, 총 예상지출, 설정한 목표 불러오기 
+    this.getGoalSettingData();
+  },
   methods: {
-    showBudget() {
-      this.showBudgetBox = true;
-      this.showSetButton = false;
+    showInputBtn() {
+      this.inputBtn=true;
+      this.putbudgetBtn=true;
+      this.modifyBtn=false;
     },
-    budgetSet() {
-      this.showBudgetBox = false;
-      this.showSetButton = true;
+    async getGoalSettingData() {
+      const response = await getGoals();
+      this.goals = response.data;
+      console.log(this.goals);
     },
-    addbudget() {
-      console.log("실행됨");
+    putbudget() {
       this.totalConsumption = 
         this.food + 
         this.culture + 
@@ -58,7 +89,7 @@ export default {
         this.errorMessage = true;
       } else {
         this.errorMessage = false;
-        this.$emit('submitGoal', {
+        this.$emit('putGoal', {
           food: this.food,
           culture: this.culture,
           life: this.life,
@@ -70,14 +101,24 @@ export default {
           ect: this.ect
         })
       }
-    }
+    },
+    showBudget() {
+      this.showBudgetBox = true;
+      this.showSetButton = false;
+    },
+    budgetSet() {
+      this.showBudgetBox = false;
+      this.showSetButton = true;
+    },
   },
   data() {
     return {
+      goals: [],
+      inputBtn: false,
       budget: 0,
+      totalConsumption: 0,
       showBudgetBox: false,
       showSetButton: true,
-      totalConsumption: 0,
       errorMessage: false,
       food: "",
       culture: "",
@@ -88,6 +129,8 @@ export default {
       traffic: "",
       beauty: "",
       ect: 0,
+      putbudgetBtn: false,
+      modifyBtn: true,
     }
   },
 }
